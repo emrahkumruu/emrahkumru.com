4f74ba5488d99216861fcaccf81e812546b25404
Emrah Kumru <emrahkumru@live.com>
2017-06-04 18:06:50 +0300

    inital commit

---
+++ source/config.js
@@ -0,0 +1,50 @@
+/**
+ * @author Bilal Cinarli
+ */
+
+(function(){
+    'use-strict';
+
+    var config = {};
+
+    config.paths = {
+        app:     'app/',
+        fonts:   'app/assets/fonts/',
+        images:  'app/assets/images/',
+        scripts: 'app/assets/scripts/',
+        styles:  'app/assets/styles/',
+        js:      'source/scripts/',
+        scss:    'source/styles/',
+        tpl:     'source/templates/',
+        svg:     'source/svg/',
+        data:    'source/data/'
+    };
+
+    config.namespace = {
+        templates : 'Rocket.templates'
+    }
+
+    config.banner = [
+        '/*! <%= pkg.name %> \n' +
+        ' *  <%= pkg.description %> \n' +
+        ' *  @author <%= pkg.author %> \n' +
+        '<% if (typeof pkg.contributors !== "undefined") { %>' +
+        '<% pkg.contributors.forEach(function(contributor) { %>' +
+        ' *          <%= contributor.name %> <<%=contributor.email %>> (<%=contributor.url %>)\n' +
+        '<% }) %>' +
+        '<% } %>' +
+        ' *  @version <%= pkg.version %> \n' +
+        ' *  @build <%= date %> \n' +
+        ' */\n'
+    ].join('');
+
+    config.libscripts = [
+        //'bower_components/dependency/js/unminified/file'
+    ];
+    
+    config.scripts = [
+        config.paths.js + '**/*.js'
+    ];
+
+    module.exports = config;
+})();
